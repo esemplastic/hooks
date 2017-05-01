e237272e4974f42b5ef21de89ab10ddf9c0cbe25
esemplastic <esemplastic2006@hotmail.com>
2017-05-01 15:00:20 +0000

    Add more helpers for payload, able to send multiple payloads, tests passed.

--- hooks_test.go
+++ hooks_test.go
@@ -2,7 +2,12 @@ package hooks
 
 import (
 	"errors"
+	"reflect"
 	"testing"
+)
+
+var (
+	hub = NewHub()
 )
 
 func TestRegisterHook(t *testing.T) {
@@ -11,21 +16,51 @@ func TestRegisterHook(t *testing.T) {
 		expectedPayload       = errors.New("Error from state1")
 	)
 
-	hub := NewHub()
-
 	hub.RegisterHook(state1, func(p PayloadWrapper) {
 		if expected, got := expectedPayload.Error(), p.First().Err().Error(); expected != got {
-			t.Fatalf("Expected error message: '%s' but got: '%s'", expected, got)
+			t.Fatalf("expected error message: '%s' but got: '%s'", expected, got)
 		}
 	})
 
 	if expected, got := 1, len(hub.hooks); expected != got {
-		t.Fatalf("Expected hooks len to be %d but got %d", expected, got)
+		t.Fatalf("expected hooks len to be %d but got %d", expected, got)
 	}
 
 	if expected, got := 1, len(hub.states); expected != got {
-		t.Fatalf("Expected states len to be %d but got %d", expected, got)
+		t.Fatalf("expected states len to be %d but got %d", expected, got)
 	}
 
 	hub.Notify(state1, expectedPayload)
+}
+
+func TestNotify(t *testing.T) {
+	var (
+		state2           State = 20
+		state3           State = 30
+		expectedPayloads       = []string{"payload1", "payload2", "payload3", "payload4"}
+	)
+
+	hub.RegisterHook(state2, func(p PayloadWrapper) {
+		if expected, got := reflect.TypeOf(expectedPayloads).Kind(), p.First().Kind(); expected != got {
+			t.Fatalf("expected to receive kind of: %#v but got: %#v", expected, got)
+		}
+	})
+
+	hub.RegisterHook(state3, func(p PayloadWrapper) {
+
+		p.Iterate(func(index int, payload SinglePayloadWrapper) {
+
+			if expected := len(expectedPayloads); expected-1 < index {
+				t.Fatalf("[%d] - exceed number of expected payloads. Expected maximum len: %d", index, expected)
+			}
+
+			if expected, got := expectedPayloads[index], payload.String(); expected != got {
+				t.Fatalf("[%d] - expected payload string to be: '%s' but got: '%s'", index, expected, got)
+			}
+		})
+	})
+
+	hub.Notify(state2, []Payload{expectedPayloads})
+	hub.Notify(state3, Payload("payload1"), Payload("payload2"), Payload("payload3"), Payload("payload4"))
+	// state3 note: This doesn't works []Payload{expectedPayloads}...)
 }
--- payload.go
+++ payload.go
@@ -1,15 +1,151 @@
 package hooks
 
 import (
+	"fmt"
 	"net"
+	"reflect"
+	"strconv"
 )
 
 type SinglePayloadWrapper struct {
 	Payload
+	typ reflect.Type
+}
+
+func (p SinglePayloadWrapper) IsNil() bool {
+	return p.Payload == nil
+}
+
+func (p SinglePayloadWrapper) Kind() reflect.Kind {
+	return reflect.TypeOf(p.Payload).Kind()
+}
+
+func (p SinglePayloadWrapper) IsKindOf(kind reflect.Kind) bool {
+	return p.Kind() == kind
 }
 
 func (p SinglePayloadWrapper) Interface() interface{} {
 	return p.Payload
+}
+
+func (p SinglePayloadWrapper) String() string {
+	if p.IsNil() {
+		return ""
+	}
+
+	if str, ok := p.Payload.(string); ok {
+		return str
+	}
+
+	return ""
+}
+
+func (p SinglePayloadWrapper) SliceString() []string {
+	if p.IsNil() {
+		return nil
+	}
+
+	if arr, ok := p.Payload.([]string); ok {
+		return arr
+	}
+
+	return nil
+}
+
+func (p SinglePayloadWrapper) Boolean() (bool, error) {
+	v := p.Payload
+	// here we could check for "true", "false" and 0 for false and 1 for true
+	// but this may cause unexpected behavior from the developer if they expecting an error
+	// so we just check if bool, if yes then return that bool, otherwise return false and an error
+	if vb, ok := v.(bool); ok {
+		return vb, nil
+	}
+
+	if vs, ok := v.(string); ok {
+		return strconv.ParseBool(vs)
+	}
+
+	return false, fmt.Errorf("unable to parse boolean of %#v", v)
+}
+
+func (p SinglePayloadWrapper) Int() (int, error) {
+	v := p.Payload
+
+	if vint, ok := v.(int); ok {
+		return vint, nil
+	}
+
+	if vstring, sok := v.(string); sok {
+		return strconv.Atoi(vstring)
+	}
+
+	return -1, fmt.Errorf("unable to parse number of %#v", v)
+}
+
+func (p SinglePayloadWrapper) Int64() (int64, error) {
+	v := p.Payload
+
+	if vint64, ok := v.(int64); ok {
+		return vint64, nil
+	}
+
+	if vint, ok := v.(int); ok {
+		return int64(vint), nil
+	}
+
+	if vstring, sok := v.(string); sok {
+		return strconv.ParseInt(vstring, 10, 64)
+	}
+
+	return -1, fmt.Errorf("unable to parse number of %#v", v)
+}
+
+func (p SinglePayloadWrapper) Float32() (float32, error) {
+	v := p.Payload
+
+	if vfloat32, ok := v.(float32); ok {
+		return vfloat32, nil
+	}
+
+	if vfloat64, ok := v.(float64); ok {
+		return float32(vfloat64), nil
+	}
+
+	if vint, ok := v.(int); ok {
+		return float32(vint), nil
+	}
+
+	if vstring, sok := v.(string); sok {
+		vfloat64, err := strconv.ParseFloat(vstring, 32)
+		if err != nil {
+			return -1, err
+		}
+		return float32(vfloat64), nil
+	}
+
+	return -1, fmt.Errorf("unable to parse number of %#v", v)
+}
+
+func (p SinglePayloadWrapper) Float64() (float64, error) {
+	v := p.Payload
+
+	if vfloat32, ok := v.(float32); ok {
+		return float64(vfloat32), nil
+	}
+
+	if vfloat64, ok := v.(float64); ok {
+		return vfloat64, nil
+	}
+
+	if vint, ok := v.(int); ok {
+		return float64(vint), nil
+	}
+
+	if vstring, sok := v.(string); sok {
+		return strconv.ParseFloat(vstring, 32)
+	}
+
+	return -1, fmt.Errorf("unable to parse number of %#v", v)
 }
 
 func (p SinglePayloadWrapper) Err() error {
@@ -29,7 +165,7 @@ func (p SinglePayloadWrapper) Listener() net.Listener {
 }
 
 type PayloadWrapper struct {
-	payloads []Payload
+	payloads []Payload // remember to convert that to simple SInglePayloadWrapper in order to reduce the compiler of creating new object structs on each call.
 }
 
 func (p PayloadWrapper) Index(idx int) SinglePayloadWrapper {
@@ -59,4 +195,10 @@ func (p PayloadWrapper) Second() SinglePayloadWrapper {
 
 func (p PayloadWrapper) Last() SinglePayloadWrapper {
 	return p.Index(len(p.payloads) - 1)
+}
+
+func (p PayloadWrapper) Iterate(visitor func(int, SinglePayloadWrapper)) {
+	for i := range p.payloads {
+		visitor(i, p.Index(i))
+	}
 }
